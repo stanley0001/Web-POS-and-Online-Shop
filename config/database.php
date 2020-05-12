4b2b0566c6283109bab384530628eaa637b34ca7
stanley <mungaistanley001@gmail.com>
2020-05-12 13:09:15 +0300

    update database

--- config/database.php
+++ config/database.php
@@ -42,8 +42,22 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-
         'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'db4free.net'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'brainstech'),
+            'username' => env('DB_USERNAME', 'stanley0001'),
+            'password' => env('DB_PASSWORD', 'stanoh001'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
+        /*'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -60,9 +74,9 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            ]) : [], 
         ],
-
+ */
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
